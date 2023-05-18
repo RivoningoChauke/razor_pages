f886bf77c9f18490daf32f54cb17a5e7805e6289
Rivoningo Chauke <mashambarm26@gmail.com>
2023-05-18 15:32:45 +0200

    add button doesn't show

--- TotorialsEU/Models/MyDbContext.cs
+++ TotorialsEU/Models/MyDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace TotorialsEU.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TotorialsEU.Models
 {
-    public class MyDbContext
+    public class MyDbContext : DbContext
     {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+            
+        }
     }
 }
--- TotorialsEU/Pages/People.cshtml.cs
+++ TotorialsEU/Pages/People.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TotorialsEU.Models;
 
 namespace TotorialsEU.Pages
 {
     public class PeopleModel : PageModel
     {
+        private readonly MyDbContext _context;
+
+        [BindProperty]
+
+        public Person NewPerson { get; set; }
+
+        public PeopleModel(MyDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+
+            _context.People.Add(NewPerson);
+
+            _context.SaveChanges();
+
+            return RedirectToPage();
         }
     }
 }
